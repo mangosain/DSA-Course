af9f6c9b3016f07324d06fdb95d243e502dab6dc
Manav Gosain <k.gosain7140@gmail.com>
2024-10-06 17:20:27 +0530

    added keypad combinations

---
+++ Chpater 20 - Backtracking/Practice Questions/KeypadCombinations.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class KeypadCombinations {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader((System.in))));
+
+        System.out.print("Enter a string of numbers: ");
+        String str = br.readLine();
+
+        combinations(str, 0, "");
+
+        System.out.println("Total Combinations: " + count);
+    }
+
+    static String[] arr = { "", "", "abc", "def", "ghi", "jkl", "mno", "pars", "wxyz" };
+    static int count = 0;
+
+    public static void combinations(String str, int i, String res) {
+        if (i == str.length()) {
+            System.out.println(res);
+            return;
+        }
+
+        int key = str.charAt(i) - '0';
+        String keyStr = arr[key];
+        for (int j = 0; j < keyStr.length(); j++) {
+            combinations(str, i + 1, res + keyStr.charAt(j));
+        }
+    }
+}
